46a6e9acbb04b1afb0f274a8570c357b76a4fb8b
Gregory <goutgregoryy@gmail.com>
2020-04-07 09:36:19 +0200

    Ex2

--- script.php
+++ script.php
@@ -1,6 +1,18 @@
 <?php
 
-$file=fopen("test.txt", "r");
+// $file=fopen("test.txt", "r");
+
+// var_dump(fgets($file));
+
+// fclose($file);
+
+$file=fopen("compteur.txt", "c");
+
+$page=["affichage.html", "accueil.html"];
+
+$max= sizeof($page);
+
+fwrite($file, "Le nombre d affichage de pages est de". $max . "pages");
 
 var_dump(fgets($file));
 
