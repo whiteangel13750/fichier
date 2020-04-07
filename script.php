e98888f9561bf612b038de1467281a60c37c9202
Gregory <goutgregoryy@gmail.com>
2020-04-07 15:20:40 +0200

    ex3

--- script.php
+++ script.php
@@ -6,13 +6,27 @@
 
 // fclose($file);
 
-$file=fopen("compteur.txt", "r+");
+// $file=fopen("compteur.txt", "r+");
 
-$pages = fgets($file);
-$pages++;
-fseek($file,0);
-fwrite($file, $pages);
+// $pages = fgets($file);
+// $pages++;
+// fseek($file,0);
+// fwrite($file, $pages);
+// fclose($file);
+
+// echo '<p>Cette page a été vue ' . $pages . ' fois !</p>';
+// 
+
+$liste=array(array("Rose", "12","2","100"),
+array("Jules","15","1","100"),
+array("Jean","6","1","100"));
+
+
+$file=fopen("index.csv", "w");
+
+fputcsv($file, $liste);
+
+var_dump($liste);
 fclose($file);
 
-echo '<p>Cette page a été vue ' . $pages . ' fois !</p>';
 ?>
